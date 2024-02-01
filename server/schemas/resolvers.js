1b9726d1d84741718a2b43b1f6b4d7be3c1605af
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-01-31 17:48:56 -0700

    mutation for create and delete users

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -29,6 +29,36 @@ const resolvers = {
     },
 
     Mutation: {
-        addUser: async (parent, {name, email, password})
-    }
-}
+        addUser: async (parent, { firstName, lastName, email, password }) => {
+            const user = await User.create({ firstName, lastName, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            };
+
+            const token = signToken(user);
+            return {token, user };
+        },
+
+        removeUser: async (parent, args, context) => {
+            if (context.user) {
+                return User.findByIdAndDelete({ _id: context.user._id });
+            }
+            throw AuthenticationError;
+        },
+    },
+};
+
+module.exports = resolvers;
