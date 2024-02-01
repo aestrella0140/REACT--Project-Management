8921476e92e6a8c8943ae3d66e95b136fe65e00e
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-01-31 17:48:07 -0700

    query for get data for user and Project

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,33 @@ const { User, Project } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
-Query: {
-    
-}
+    Query: {
+        users: async () => {
+            return User.find();
+        },
+
+        user: async (parent, { userId }) => {
+            return User.findOne({ _id: userId });
+        },
+
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+
+            throw AuthenticationError;
+        },
+
+        getProjects: async () => {
+            return Project.find();
+        },
+
+        getProject: async (parent, { projectId }) => {
+            return Project.findOne({ _id: projectId })
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, {name, email, password})
+    }
 }
