27208ae3dc028af4e72a37963e0ca679c80827fa
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-02-06 17:52:37 -0700

    setup apollo provider an httplink

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,30 @@
-import { useState } from 'react'
+import { Outlet } from 'react-router-dom';
+import {
+  AppolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new AppolloClient({
+  link: authLink.concat(httpLink), 
+  cache: new InMemoryCache(),
+});
 
 import './App.css'
 
@@ -6,10 +32,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ApolloProvider client={client}>
       
-    </>
+    </ApolloProvider>
   )
 }
 
-export default App
+export default App;
