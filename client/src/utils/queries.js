2176e971f0e8409591355231aebf4963132fb44d
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-02-07 17:41:38 -0700

    queries for client side

---
+++ client/src/utils/queries.js
@@ -0,0 +1,57 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USERS = gql`
+query allUsers {
+    users {
+      _id
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER = gql`
+query singleUser ($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+query me {
+    me {
+      _id
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const QUERY_PROJECTS = gql`
+query projects {
+    projects {
+      _id
+      Users {
+        _id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+export const QUERY_SINGLE_PROJECT = gql`
+query singleProject($projectId: ID!) {
+    project(projectId: $projectId) {
+      _id
+      Users {
+        _id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
