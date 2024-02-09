6713e401149e99716674156d537243c6e4f2bc77
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-02-08 18:16:30 -0700

    initial setup for signup page using formik

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,4 +1,56 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 
 import { useMutation } from '@apollo/client';
+
+import Auth from '../utils/auth';
+
+const SignupForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor='firstName'>first Name</label>
+            <input
+                type='text'
+                name='firstName'
+                id='firstName'
+                onChange={formik.handleChange}
+                value={formik.values.lastName} />
+
+            <label htmlFor='lastName'>Last Name</label>
+            <input
+                type='text'
+                name='lastName'
+                id='lastName'
+                onChange={formik.handleChange}
+                value={formik.values.email} />
+
+            <label htmlFor='email'>email</label>
+            <input
+                type='email'
+                name='email'
+                id='email'
+                onChange={formik.handleChange}
+                value={formik.values.firstName} />
+
+            <label htmlFor='password'>Password</label>
+            <input
+                type='password'
+                name='password'
+                id='pwd'
+                onChange={formik.handleChange}
+                value={formik.values.password} />
+
+        </form>
+    )
+}
