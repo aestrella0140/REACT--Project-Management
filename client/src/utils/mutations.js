3b1f060e8bb394cb5bde872f9edf2dbd9728f30d
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-02-08 17:36:15 -0700

    mutations for rest methods for project

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,7 +14,7 @@ mutation addUser($firstName: String!, $lastName: String!, $email: String!, $pass
 `;
 
 export const LOGIN_USER = gql`
-mutation Mutation($email: String!, $password: String!) {
+mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
@@ -24,4 +24,61 @@ mutation Mutation($email: String!, $password: String!) {
       }
     }
   }
+`;
+
+export const REMOVE_USER = gql`
+mutation removeUser {
+  removeUser {
+    _id
+    firstName
+    lastName
+  }
+}
+`;
+
+export const CREATE_PROJECT = gql`
+mutation createProject($title: String!, $description: String!, $priority: String!, $users: [ID]!, $dependencies: String!, $status: String) {
+  createProject(title: $title, description: $description, priority: $priority, Users: $users, dependencies: $dependencies, Status: $status) {
+    _id
+    title
+    description
+    Status
+    priority
+    Users {
+      _id
+      firstName
+      lastName
+    }
+  }
+}
+`;
+
+export const REMOVE_PROJECT = gql`
+mutation removeProject {
+  removeProject {
+    _id
+    Users {
+      _id
+      firstName
+      lastName
+    }
+  }
+}
+`;
+
+export const UPDATE_PROJECT = gql`
+mutation updateProject($title: String!, $description: String!, $priority: String!, $users: [ID]!, $dependencies: String!, $status: String) {
+  updateProject(title: $title, description: $description, priority: $priority, Users: $users, dependencies: $dependencies, Status: $status) {
+    _id
+    title
+    description
+    Status
+    priority
+    Users {
+      _id
+      firstName
+      lastName
+    }
+  }
+}
 `;
