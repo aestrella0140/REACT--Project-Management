3665bb6b60032f34ab2c8dfd377abb4da86308c3
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-02-21 17:00:42 -0700

    profile to display personal project and other data

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,37 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import project from '../components/card/project';
+
+import { QUERY_SINGLE_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
+
+const Profile = () => {
+    const { userId } = useParams();
+
+    const { loading, data } = useQuery(
+        userId ? QUERY_SINGLE_USER : QUERY_ME,
+        {
+            variables: { userId: userId },
+        }
+    );
+
+    const profile = data?.me || data?.profile || {};
+
+    if (Auth.loggedIn() && Auth.getUser().data._id === userId) {
+        return <Navigate to="me" />;
+    }
+
+    if(loading) {
+        return <div>loading....</div>;
+    }
+
+    if (!profile?.name) {
+        return(
+            <h4>
+                You need to be logged in to see your profile page. Use the navigation links above to 
+                sign in or sign up!
+            </h4>
+        );
+    }
+}
