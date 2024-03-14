762e3e49b6305310573bfa02bdb7c2a5f04c85ba
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-03-13 18:00:48 -0700

    reducers for update projects, categories, current category

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,29 @@
-export const UPDATE_PROJECT = "UPDATE_PROJECTS";
+import { UPDATE_PROJECTS,
+     UPDATE_CATEGORIES,
+      UPDATE_CURRENT_CATEGORY
+     } from "./actions";
 
-export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
-export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+     export const reducer = (state, action) => {
+        switch (action.type) {
+            case UPDATE_PROJECTS:
+                return {
+                    ...state,
+                    projects: [...action.projects],
+                };
+
+                case UPDATE_CATEGORIES: 
+                return {
+                    ...state,
+                    categories: [...action.categories],
+                };
+
+                case UPDATE_CURRENT_CATEGORY: 
+                return {
+                    ...state,
+                    currentCategory: action.currentCategory
+                }
+
+                default: 
+                return state;
+        }
+     };
