362158977f976405311ab98a766115bc00fb09b2
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-04-04 15:28:12 -0700

    setup for card and css for position

--- client/src/components/card/project.jsx
+++ client/src/components/card/project.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFormik ,Formik, Form, Field } from "formik";
+import { useFormik, Formik, Form, Field } from "formik";
 
 import { useMutation } from "@apollo/client";
 import { CREATE_PROJECT } from "../../utils/mutations";
@@ -77,64 +77,71 @@ const projectForm = () => {
       >
         {() => (
           <Form className="project-form">
-            <label htmlFor="title">Title</label>
-            <Field
-              type="title"
-              name="title"
-              id="title"
-              placeholder="Enter title here"
-            />
+            <div className="top">
+              <label htmlFor="title">Title</label>
+              <Field
+                type="title"
+                name="title"
+                id="title"
+                placeholder="Enter title here"
+              />
 
-            <label htmlFor="description">Description</label>
-            <Field
-              type="description"
-              name="description"
-              id="description"
-              placeholder="Enter description here"
-            />
+              <label htmlFor="category">Category</label>
+              <Field as="select" name="category" id="category">
+                <option value="">select</option>
+                <option value="option1">AC</option>
+                <option value="option2">SWaGA</option>
+                <option value="option3">Viatorr</option>
+              </Field>
 
-            <label htmlFor="Status">Status</label>
-            <Field
-              type="text"
-              name="Status"
-              id="Status"
-              placeholder="Enter Status here"
-              
-            />
+              <label htmlFor="Users">Users</label>
+              <Field
+                type="text"
+                name="Users"
+                id="Users"
+                placeholder="Add Users to project"
+              />
+            </div>
 
-            <label htmlFor="priority">Priority</label>
-            <Field
-              type="text"
-              name="priority"
-              id="priority"
-              placeholder="Choose priority of project"
-              
-            />
+            <div className="middle">
+              <label htmlFor="description">Description</label>
+              <Field
+                type="description"
+                name="description"
+                id="description"
+                placeholder="Enter description here"
+              />
 
-            <label htmlFor="Users">Users</label>
-            <Field
-              type="text"
-              name="Users"
-              id="Users"
-              placeholder="Add Users to project"
-              
-            />
+              <label htmlFor="Status">Status</label>
+              <Field
+                type="text"
+                name="Status"
+                id="Status"
+                placeholder="Enter Status here"
+              />
+            </div>
 
-            <label htmlFor="dependencies">Dependencies</label>
-            <Field
-              type="text"
-              name="dependencies"
-              id="dependencies"
-              placeholder="Add dependencies to project"
-            />
+            <div className="bottom">
+              <label htmlFor="priority">Priority</label>
+              <Field
+                type="text"
+                name="priority"
+                id="priority"
+                placeholder="Choose priority of project"
+              />
 
-            <label htmlFor="category">Category</label>
-            <Field as="select" name="category" id="category">
-              <option value="">select</option>
-              <option value="option1">AC</option>
-              <option value="option2">SWaGA</option>
-              <option value="option3">Viatorr</option>
-            </Field>
+              <div>
+                <label htmlFor="dependencies">Dependencies</label>
+                <Field
+                  type="text"
+                  name="dependencies"
+                  id="dependencies"
+                  placeholder="Add dependencies to project"
+                />
+              </div>
+            </div>
+
+            <button type="submit">Submit</button>
           </Form>
         )}
       </Formik>
