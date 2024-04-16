5e985a041d33e412a17bd7977ee0247051601aac
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-04-15 17:58:31 -0700

    flip card creation using css

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -19,32 +19,49 @@ const LandingPage = () => {
         <div className="inner">
           <img className="img-fluid width-50" src={goreLogo} alt="Gore Logo" />
         </div>
-        <h1 className="cover-heading">
-          Welcome to our Continuous team!
-        </h1>
-
-        <p className="lead">
-          W.L Gore's continuous improvement process is all about fostering team
-          collaboration, encouraging innovative thinking, and relentlessly
-          pursuing incremental enhancement to products and processes. Its a
-          culture where everyones voice matters, and the aim is to create
-          high-quality products that lead the market.
-        </p>
+        <h1 className="cover-heading">Welcome to our Continuous team!</h1>
+        <div className="d-flex flex-column justify-content-center align-items-center vh-50">
+          <p className="w-50 lead">
+            W.L Gore's continuous improvement process is all about fostering
+            team collaboration, encouraging innovative thinking, and
+            relentlessly pursuing incremental enhancement to products and
+            processes. Its a culture where everyones voice matters, and the aim
+            is to create high-quality products that lead the market.
+          </p>
+        </div>
         <p className="lead"></p>
 
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-        <p className="text-center  w-50 p-3">join a community of innovative thinkers dedicated to continuous improvement. You'll have access to our resources, be able to participate in discussions, share your ideas, and contribute to our ongoing efforts to enhance our products and processes.</p>
-        <div className="d-flex justify-content-center m-2">
-          <Link to="/Signup">
-            <button className="">Signup</button>
-          </Link>
+        {/*want 3 flip cards here */}
+        <div className="d-flex justify-content-around">
+        {[1, 2, 3].map(i => (
+            <div className="card" style={{width: '18rem' }}>
+                <div className="card-body">
+                    <h5 className="card-title">Card title {i}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Card Subtitle</h6>
+
+                </div>
+            </div>
+        ))}
         </div>
-        <div>
-          <Link to='/Login'>
-            <button>Login</button>
-          </Link>
+
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+          <p className="text-center  w-50 p-3">
+            join a community of innovative thinkers dedicated to continuous
+            improvement. You'll have access to our resources, be able to
+            participate in discussions, share your ideas, and contribute to our
+            ongoing efforts to enhance our products and processes.
+          </p>
+          <div className="d-flex justify-content-center m-2">
+            <Link to="/Signup">
+              <button className="">Signup</button>
+            </Link>
+          </div>
+          <div>
+            <Link to="/Login">
+              <button>Login</button>
+            </Link>
+          </div>
         </div>
-    </div>
       </main>
     </div>
   );
