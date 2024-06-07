20866c3949b65df2d8d9cd93825293ab404f991c
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-06-06 17:04:30 -0700

    dataProvider handlers REST methods for category and projects

--- client/src/components/dataProvider/dataProvider.jsx
+++ client/src/components/dataProvider/dataProvider.jsx
@@ -1,47 +1,88 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://my.graphql.api',
-    cache: new InMemoryCache()
+  uri: "http://my.graphql.api",
+  cache: new InMemoryCache(),
 });
 // mutations
 const REMOVE_USER = gql`
-mutation removeUser {
+  mutation removeUser {
     removeUser {
-        _id
-        firstName
-        lastName
+      _id
+      firstName
+      lastName
     }
-}
+  }
 `;
 
 const CREATE_CATEGORY = gql`
-mutation CreateCategory($type: String!) {
+  mutation CreateCategory($type: String!) {
     createCategory(type: $name) {
-        id
-        type
+      id
+      type
     }
-}
+  }
 `;
 
 const UPDATE_CATEGORY = gql`
-mutation CreateCategory($type: String!) {
+  mutation CreateCategory($type: String!) {
     createCategory(type: $type) {
-        id
-        type
+      id
+      type
     }
+  }
+`;
+
+const CREATE_PROJECT = gql`
+mutation createProject($title: String!, $description: String!, $priority: String!, $Users: String!, $dependencies: String!, $category: ID!, $status: String) {
+  createProject(title: $title, description: $description, priority: $priority, Users: $Users, dependencies: $dependencies, category: $category, Status: $status) {
+    _id
+    Users 
+    Status
+    category {
+      _id
+      type
+    }
+    dependencies
+    description
+    priority
+    title
+  }
 }
 `;
 
-export const UPDATE_PROJECT = gql`
-mutation updateProject($title: String!, $description: String!, $priority: String!, $users: String!, $dependencies: String!, $status: String) {
-  updateProject(title: $title, description: $description, priority: $priority, Users: $users, dependencies: $dependencies, Status: $status) {
+const UPDATE_PROJECT = gql`
+  mutation updateProject(
+    $title: String!
+    $description: String!
+    $priority: String!
+    $users: String!
+    $dependencies: String!
+    $status: String
+  ) {
+    updateProject(
+      title: $title
+      description: $description
+      priority: $priority
+      Users: $users
+      dependencies: $dependencies
+      Status: $status
+    ) {
+      _id
+      title
+      description
+      Status
+      priority
+      Users
+    }
+  }
+`;
+
+const REMOVE_PROJECT = gql`
+mutation removeProject {
+  removeProject {
     _id
-    title
-    description
-    Status
-    priority
-    Users
+    Users 
   }
 }
 `;
@@ -49,7 +90,7 @@ mutation updateProject($title: String!, $description: String!, $priority: String
 // queries
 
 const QUERY_USERS = gql`
-query allUsers {
+  query allUsers {
     users {
       _id
       firstName
@@ -59,23 +100,23 @@ query allUsers {
 `;
 
 const QUERY_PROJECTS = gql`
-query getProjects($category: ID) {
-  projects(category: $category) {
-    _id
-    title
-    description
-    priority
-    dependencies
-    Status
-    category {
+  query getProjects($category: ID) {
+    projects(category: $category) {
       _id
+      title
+      description
+      priority
+      dependencies
+      Status
+      category {
+        _id
+      }
     }
   }
-}
 `;
 
 const QUERY_SINGLE_PROJECT = gql`
-query singleProject($projectId: ID!) {
+  query singleProject($projectId: ID!) {
     project(projectId: $projectId) {
       _id
       Users {
@@ -88,16 +129,61 @@ query singleProject($projectId: ID!) {
 `;
 
 const QUERY_CATEGORIES = gql`
-{
-  categories {
-    _id
-    type
+  {
+    categories {
+      _id
+      type
+    }
   }
-}
 `;
 
 const dataProvider = {
+  getList: async (resource, params) => {
+    if (resource === 'projects') {
+      const { data } = await client.query({
+        query: QUERY_PROJECTS,
+        variables: { category: params.filter.category },
+      });
+      return { data: data.projects };
+    } else if (resource === 'categories') {
+      const { data } = await client.query({ query: QUERY_CATEGORIES });
+      return { data: data.categories };
+    }
+  },
 
+  getOne: async (resource, params) => {
+    if (resource === 'projects') {
+      const { data } = await client.query({ query: QUERY_SINGLE_PROJECT, variables: { projectId: params.id} });
+      return { data: data.project};
+    }
+  },
+
+  create: async (resource, params) => {
+    if (resource === 'projects') {
+      const { data } = await client.mutate({ mutation: CREATE_PROJECT, variables: params.data });
+      return { data: data.createProject };
+    } else if (resource === 'categories') {
+      const { data } = await client.mutate({ mutation: CREATE_CATEGORY, variables: params.data});
+      return { data: data.createCategory };
+    }
+  },
+
+  update: async (resource, params) => {
+    if (resource === 'projects') {
+      const { data } = await client.mutate({ mutation: UPDATE_PROJECT, variables: params.data });
+      return { data: data.updateProject };
+    } else if (resource === 'categories') {
+      const { data } = await client.mutate({ mutation: CREATE_CATEGORY, variables: params.data });
+      return { data: data.updateCategory};
+    }
+  },
+
+  delete: async (resource, params) => {
+    if (resource === 'projects') {
+      await client.mutate({ mutation: REMOVE_PROJECT, variables: { id: params.id} });
+      return { data: params.previousData};
+    } 
+  }
 };
 
 export default dataProvider;
